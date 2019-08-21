949ccd1a0522d9037f96524be4002903b2401f2d
Guilherme <gabanelli.guilherme@gmail.com>
2019-08-20 21:19:20 -0300

    T2

--- AulaProcessos/src/controller/ProcController.java
+++ AulaProcessos/src/controller/ProcController.java
@@ -1,6 +1,8 @@
 package controller;
 
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class ProcController {
 	
@@ -16,7 +18,39 @@ public class ProcController {
 	public void chamaProcesso(String processo) {
 		try {
 			Runtime.getRuntime().exec(processo);
-		} catch (IOException e) {
+		} catch (Exception e) {
+			if(e.getMessage().contains("740")) {
+				//cmd /c processo
+				StringBuffer buffer = new StringBuffer();
+				buffer.append("cmd /c");
+				buffer.append(" ");
+				buffer.append(processo);
+				try {
+					Runtime.getRuntime().exec(buffer.toString());
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}else {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void leProcesso(String nomeProcesso) {
+		try {
+			Process processo = Runtime.getRuntime().exec(nomeProcesso);
+			InputStream fluxo = processo.getInputStream();
+			InputStreamReader leitor = new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(leitor);
+			String linha = buffer.readLine();
+			while(linha != null) {
+				System.out.println(linha);
+				linha = buffer.readLine();
+			}
+			buffer.close();
+			leitor.close();
+			fluxo.close();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
--- AulaProcessos/src/view/Principal.java
+++ AulaProcessos/src/view/Principal.java
@@ -8,6 +8,12 @@ public class Principal {
 			ProcController proc = new ProcController();
 			String os = proc.os();
 			System.out.println(os);
+			
+			//String processo = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
+			//proc.chamaProcesso(processo);
+			
+			String processo = "PING -t www.google.com.br";
+			proc.leProcesso(processo);
 	}
 
 }
